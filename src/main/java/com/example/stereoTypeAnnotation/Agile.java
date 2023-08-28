284e08994ef9645d55217fa6bab4862b6e45418d
murat <murat.zeybek@sky.uk>
2023-08-28 10:19:01 +0100

    add stereotype

---
+++ src/main/java/com/example/stereoTypeAnnotation/Agile.java
@@ -0,0 +1,10 @@
+package com.example.stereoTypeAnnotation;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Agile {
+    public void getTeachingHours(){
+        System.out.println("Weekly teaching hours" + 10);
+    }
+}
---
+++ src/main/java/com/example/stereoTypeAnnotation/ConfigCourse.java
@@ -0,0 +1,11 @@
+package com.example.stereoTypeAnnotation;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@ComponentScan
+//@ComponentScan(basePackages = "com.example")
+public class ConfigCourse {
+
+}
