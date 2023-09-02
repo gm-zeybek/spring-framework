fbc3a08449e3cdd51beb1acea954336521aed42e
murat <murat.zeybek@sky.uk>
2023-09-02 22:53:39 +0100

    added DI with lombok

--- spring-review-IoC/src/main/java/stereotype_annotation/model/DataStructures.java
+++ spring-review-IoC/src/main/java/stereotype_annotation/model/DataStructures.java
@@ -1,13 +1,15 @@
 package stereotype_annotation.model;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-
+@AllArgsConstructor// or @Data with @NonNull constructor
 @Component
 public class DataStructures {
+    ExtraHours extraHours;
 
     public void getTotalHours(){
-        System.out.println("total hours : " + 10);
+        System.out.println("total hours : " + (10 + extraHours.getHours()));
     }
 }
