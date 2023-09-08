b04f27da7fee102085d99a72f555b4effed94177
murat <murat.zeybek@sky.uk>
2023-09-08 21:10:46 +0100

    add mvc model using argument as object

--- spring-08-MVC-model/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-08-MVC-model/src/main/java/com/cydeo/controller/StudentController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class StudentController {
     @RequestMapping("/welcome")
@@ -11,6 +15,17 @@ public class StudentController {
         // values coming from inside the method and not visible from another method
         model.addAttribute("name", "Cydeo");
         model.addAttribute("course", "MVC");
+        model.addAttribute("lesson", "data");
+        BigInteger studenId = BigInteger.valueOf((long) (Math.random()*1000));
+        model.addAttribute("studentId", studenId);
+
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(3);
+        list.add(4);
+        list.add(2);
+        list.add(8);
+        model.addAttribute("numbers", list);
+
         return "/student/welcome"; // NO HTML ANYMORE SINCE IT IS USING THYMELEAF
     }
 }
