861cb2984e6771598d33bac32e4253bfec90af05
murat <murat.zeybek@sky.uk>
2023-09-23 09:52:15 +0100

    added thymeleaf test

---
+++ spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -0,0 +1,28 @@
+package com.cydeo.controller;
+
+import com.cydeo.bootstrap.DataGenerator;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+
+
+    @RequestMapping("/register")
+    public String register(Model model) {
+
+
+        model.addAttribute("students", DataGenerator.createStudent());
+        return "student/register";
+    }
+
+    @RequestMapping("/welcome")
+    public String welcome(){
+
+        return "student/welcome";
+    }
+}
