1ac184aee01d4fce56b9bd11ff79e08d416554dc
murat <murat.zeybek@sky.uk>
2023-09-23 20:52:02 +0100

    started form

--- spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -3,24 +3,28 @@ package com.cydeo.controller;
 import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/student")
+@RequestMapping("/student")   // @GetMapping is method level applied not class level
 public class StudentController {
 
 
-    @RequestMapping("/register")
+//    @RequestMapping("/register")
+    @GetMapping("/register")  // same as requestmapping
     public String register(Model model) {
 
 
         model.addAttribute("students", DataGenerator.createStudent());
+
         return "student/register";
     }
 
-    @RequestMapping("/welcome")
+//    @RequestMapping("/welcome")
+    @GetMapping("/welcome")  // same with requestmapping
     public String welcome(){
 
         return "student/welcome";
