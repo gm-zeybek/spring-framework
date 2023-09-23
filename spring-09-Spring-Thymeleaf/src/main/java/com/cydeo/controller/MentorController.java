4b66903d498b7a1c8547935913abbe26112c243a
murat <murat.zeybek@sky.uk>
2023-09-23 22:39:02 +0100

    form is done

--- spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -4,6 +4,7 @@ import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,12 +20,15 @@ public class MentorController {
         model.addAttribute("mentor",new Mentor());
         List<String> batchList = Arrays.asList("JD1", "JD2", "EU1", "EU2");
         model.addAttribute("batchList", batchList);
-        return "mentor/mentor-register";
+        return "/mentor/mentor-register";
     }
-    @PostMapping("/confirm")
-    public String postForm() {
 
-        return "mentor/confirmation";
+    @PostMapping("/confirm")
+    public String postForm(@ModelAttribute("mentor") Mentor mentor, Model model) { // ModelAttribute("<attribute>") accept attribute looking for
+
+        System.out.println(mentor.toString());
+//        return "/mentor/confirmation";
+        return "redirect:/mentor/register";  // redirect: allows open fresh form again
     }
 
 }
