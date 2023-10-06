4044daee2eedc2e47a7d8c5ee384d48529857839
murat <murat.zeybek@sky.uk>
2023-10-06 21:29:57 +0100

    deneme

---
+++ project-01-ticketing-project/src/main/java/com/cydeo/ticketingProject/Project01TicketingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.ticketingProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project01TicketingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project01TicketingProjectApplication.class, args);
+    }
+
+}
---
+++ project-01-ticketing-project/src/test/java/com/cydeo/ticketingProject/Project01TicketingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.ticketingProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project01TicketingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
