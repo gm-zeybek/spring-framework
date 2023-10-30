8c7c07406bd95853e98c2ac3c080bd8e8d5db7ed
murat <murat.zeybek@sky.uk>
2023-10-30 19:00:40 +0000

    ORM started

--- spring-10-ORM-tableCreating/src/main/resources/data.sql
+++ spring-10-ORM-tableCreating/src/main/resources/data.sql
@@ -1,2 +1,7 @@
-INSERT INTO EMPLOYEES(id,name)
-VALUES(1,'Mike Smith')
+-- INSERT INTO EMPLOYEES(id,name),
+-- VALUES(1,'Mike Smith')
+
+
+
+INSERT INTO STUDENT(first_name, last_name, email)
+VALUES('OZZY','SMITH','OZZY@CYDEO.COM')
