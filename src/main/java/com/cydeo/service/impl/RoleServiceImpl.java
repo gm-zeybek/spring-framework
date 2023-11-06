9756701051d87a2135b05230033bd6e3ba0efec6
murat <murat.zeybek@sky.uk>
2023-11-06 22:38:50 +0000

    ticketing app orm

--- src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -1,25 +1,32 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.RoleDTO;
+import com.cydeo.entity.Role;
+import com.cydeo.mapper.RoleMapper;
 import com.cydeo.repository.RoleRepository;
 import com.cydeo.service.RoleService;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class RoleServiceImpl implements RoleService {
 
     private final RoleRepository roleRepository;
+    private final RoleMapper roleMapper;
 
-    public RoleServiceImpl(RoleRepository roleRepository) {
+    public RoleServiceImpl(@Lazy RoleRepository roleRepository, RoleMapper roleMapper) {
         this.roleRepository = roleRepository;
+        this.roleMapper = roleMapper;
     }
 
     @Override
     public List<RoleDTO> getAllRoles() {
-        List<Role> listAllRole  = roleRepository.findAll();
-        return listAllRole;
+        List<Role> roleList  = roleRepository.findAll();
+        List<RoleDTO> roleDTOList = roleList.stream().map(role -> roleMapper.convertToDTO(role)).collect(Collectors.toList());
+        return roleDTOList;
     }
 
     @Override
