cb2437bb608ecf84d95c412c5482b16287740d17
murat <murat.zeybek@sky.uk>
2023-11-06 20:17:00 +0000

    started ticketing app orm

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@MappedSuperclass
 public class BaseEntity {
 
     private Long id;
