a1b91fc0a34dcaf659b70c50b0d53dfe098f8644
murat <murat.zeybek@sky.uk>
2023-11-19 06:48:31 +0000

    cinema MovieCinema drive query added

--- spring-13-ORM-cinemaApplication/src/main/java/com/cydeo/repository/GenreRepository.java
+++ spring-13-ORM-cinemaApplication/src/main/java/com/cydeo/repository/GenreRepository.java
@@ -2,7 +2,10 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
@@ -10,9 +13,12 @@ public interface GenreRepository extends JpaRepository<Genre, Long> {
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query that return all genres
+    @Query("SELECT g FROM Genre g")
+    List<Genre> findAllGenres();
 
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query that returns genres by containing name
-
+    @Query(value = "SELECT * FROM TABLE ( genre ) WHERE name ILIKE CONCAT('%','?1','%') OR sponsor_name ILIKE CONCAT('%','?1','%')", nativeQuery = true)
+    List<Genre> findAllByNameContaining(String name);
 }
--- spring-13-ORM-cinemaApplication/src/main/java/com/cydeo/repository/MovieCinemaRepository.java
+++ spring-13-ORM-cinemaApplication/src/main/java/com/cydeo/repository/MovieCinemaRepository.java
@@ -1,25 +1,41 @@
 package com.cydeo.repository;
 
+import com.cydeo.entity.Cinema;
+import com.cydeo.entity.Movie;
 import com.cydeo.entity.MovieCinema;
+import lombok.Data;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
 
 public interface MovieCinemaRepository extends JpaRepository<MovieCinema, Long> {
 
     // ------------------- DERIVED QUERIES ------------------- //
 
     //Write a derived query to read movie cinema with id
+    Optional<MovieCinema> findById(Long id);
 
     //Write a derived query to count all movie cinemas with a specific cinema id
+    Integer countAllByCinemaId(Long id);
 
     //Write a derived query to count all movie cinemas with a specific movie id
+    Integer countAllByMovieId(Long id);
 
     //Write a derived query to list all movie cinemas with higher than a specific date
+    List<MovieCinema> findAllByDateTimeAfter(LocalDateTime dateTime);
 
     //Write a derived query to find the top 3 expensive movies
+    List<Movie> getTop3ByOrderByMovie_PriceDesc();
 
     //Write a derived query to list all movie cinemas that contain a specific movie name
+    List<MovieCinema> findAllByMovie_NameContainingIgnoreCase(String movieName);
 
     //Write a derived query to list all movie cinemas in a specific location
+    List<MovieCinema> getAllByCinema_Location_Name(String name);
 
     // ------------------- JPQL QUERIES ------------------- //
 
