36a7ac87019668e728f0fdf77fd70f8b996ce923
murat <murat.zeybek@sky.uk>
2023-12-04 20:04:07 +0000

    rest

---
+++ Spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -0,0 +1,34 @@
+package com.cydeo.controller;
+
+
+import com.cydeo.dto.CourseDTO;
+import com.cydeo.service.CourseService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Controller// either only controller + responseBody = ResponseController
+@RestController
+@RequestMapping("/courses")
+public class CourseController {
+
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+
+    @GetMapping
+    @ResponseBody
+    public List<CourseDTO> getCourse(){
+        List<CourseDTO> list = courseService.getCourses();
+
+        return list;
+
+    }
+}
