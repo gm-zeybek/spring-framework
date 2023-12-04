93601e7ac138603b8e93649111f7f31fac08676b
murat <murat.zeybek@sky.uk>
2023-12-04 22:06:45 +0000

    jsonignore class level and field level added

--- Spring-15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseWrapper.java
+++ Spring-15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseWrapper.java
@@ -5,7 +5,9 @@ import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.service.CourseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.StreamingHttpOutputMessage;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +28,11 @@ public class CourseController_ResponseWrapper {
                 .status(HttpStatus.ACCEPTED)
                 .header("Version", "Cydeo V3")
                 .body(new ResponseWrapper("successfully retrieved", courseService.getCourses()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseWrapper> getCourseById(@PathVariable Long id){
+        return ResponseEntity
+                .ok(new ResponseWrapper("course: " + id + " retrieved, ", courseService.getCourseById(id)));
     }
 }
