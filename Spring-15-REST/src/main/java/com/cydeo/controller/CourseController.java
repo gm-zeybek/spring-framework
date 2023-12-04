133fd18dc3f6b6cbdb61e391401febbcf532bc82
murat <murat.zeybek@sky.uk>
2023-12-04 20:40:19 +0000

    rest added more endpoint

--- Spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
+++ Spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -3,12 +3,11 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.CourseDTO;
 import com.cydeo.service.CourseService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller// either only controller + responseBody = ResponseController
+//@Controller// either only controller + responseBody = ResponseController
 @RestController
 @RequestMapping("/courses")
 public class CourseController {
@@ -32,5 +31,15 @@ public class CourseController {
     @GetMapping("/{id}")
     public CourseDTO getCourseById(@PathVariable("id") Long id){
         return courseService.getCourseById(id);
+    }
+
+    @GetMapping("/category/{name}")
+    public List<CourseDTO> getCourseByCategery(@PathVariable("name") String category) {
+        return courseService.getCoursesByCategory(category);
+    }
+
+    @PostMapping
+    public CourseDTO createCourse(@RequestBody CourseDTO course){
+        return courseService.createCourse(course);
     }
 }
