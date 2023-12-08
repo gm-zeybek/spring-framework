bf544c89ae2efa985723eef98b46ab63d233abd8
murat <murat.zeybek@sky.uk>
2023-12-08 19:35:29 +0000

    Feign Client consuming

--- spring-17-REST-consumingAPIs/src/main/java/com/cydeo/controller/Consume_RestTemplate.java
+++ spring-17-REST-consumingAPIs/src/main/java/com/cydeo/controller/Consume_RestTemplate.java
@@ -2,11 +2,14 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.User;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 
 @RestController
@@ -32,6 +35,22 @@ public class Consume_RestTemplate {
     public Object getUser(@PathVariable("id") Integer id){  // return object
         String URL = URI + "/{id}";                         // third party url + /{id}
         return restTemplate.getForObject(URL, Object.class, id);
+    }
+
+
+    @GetMapping("/test")
+    public ResponseEntity<Object> consumeFromDummyApi(){
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.set("app-id","6298ebfecd0551211fce37a6");
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<Object> response = restTemplate.exchange("https://dummyapi.io/data/v1/user?limit=10", HttpMethod.GET,entity,Object.class);
+
+        return response;
+
     }
 
 }
